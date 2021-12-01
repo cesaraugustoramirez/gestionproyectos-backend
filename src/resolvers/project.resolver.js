fc05d262bfd8067b92316af6001afc1b82fdd7e2
Luis F. Ramírez O <luisfernandoramirez@gmail.com>
2021-11-30 21:09:50 -0500

    Update

---
+++ src/resolvers/project.resolver.js
@@ -0,0 +1,34 @@
+import Projects from "../models/projects.model.js";
+import Users from "../models/users.model.js";
+import Enrollements from "../models/enrollments.model.js";
+
+const allProjects = async () => {
+  const projects = await Projects.find();
+  return projects;
+};
+
+const project = async (parent, args) => {
+  const user = await Projects.findById(args._id);
+  return user;
+};
+
+const leader = async (parent) => {
+  const user = await Users.findById(parent.leader_id);
+  return user;
+};
+
+const enrollments = async (parent) => {
+  const enrollments = await Enrollements.find({ project_id: parent._id.toString() });
+  return enrollments;
+}
+
+export default {
+  projectQueries: {
+    allProjects,
+    project,
+  },
+  Project: {
+    leader,
+    enrollments,
+  }
+};
